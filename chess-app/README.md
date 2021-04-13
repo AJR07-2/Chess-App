ca9642ed56d11899ddb0c7159cfbd565b9ae0bed
Ang Jun Ray <angjunray@live.com>
2021-04-13 16:47:37 +0800

    added some images and more code

--- chess-app/README.md
+++ chess-app/README.md
@@ -1,1 +1,7 @@
 # Chess App (WIP)
+
+
+
+---
+## References
+1. Chess set icons provided by: https://iconscout.com/icon-pack/chess# 
---
+++ chess-app/src/chessboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './chessboard.css';
+
+function chessboard() {
+  return (
+    <div className="board">
+      <img className="chessBoard"
+        src="images/chessboard.png"
+        alt="">
+      </img>
+    </div>
+  );
+}
+
+export default chessboard;
